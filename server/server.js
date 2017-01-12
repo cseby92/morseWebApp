eb5500c2d1462f359c51703ed314c5683381919a
Sebastian Chikan <cseby92@gmail.com>
2017-01-12 22:03:48 +0100

    Refactoring

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@
 let express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var fs = require('fs');
 var path = require('path');
 var UserHandler = require('./userhandler');
 var decoder = require('./morsedecoder');
@@ -22,7 +21,7 @@ app.post('/users', function (req, res) {
         res.status(400).json();
     } else {
         app.handler.addUser(req.body);
-            res.status(200).json({
+        res.status(200).json({
             token: UserHandler.encodeUserName(req.body.username)
         });
     }
@@ -52,27 +51,27 @@ app.post('/users/:username/messages', function (req, res) {
 });
 
 app.get('/users/:username/messages', (req, res) => {
-    if(!req.get('X-Auth') || !app.handler.checkIfAuthExists(req.get('X-Auth')))
+    if (!req.get('X-Auth') || !app.handler.checkIfAuthExists(req.get('X-Auth')))
         res.status(401).json();
-    else if(app.handler.searchUserByAuth(req.get('X-Auth')) !== req.params.username)
+    else if (app.handler.searchUserByAuth(req.get('X-Auth')) !== req.params.username)
         res.status(403).json();
-    else{
+    else {
         res.status(200).json(app.handler.getUserMessages(req.params.username));
 
     }
-            
+
 });
 
-app.get('/users', (req, res) =>{
+app.get('/users', (req, res) => {
     res.status(200).json(app.handler.listUsers());
 });
 
-app.get('/',  (req, res) =>  {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/../public', 'index.html'));
 });
 
 
-app.listen(process.env.PORT || 3000,  () => {
+app.listen(process.env.PORT || 3000, () => {
     app.init();
     console.log('Server started in the port: ', process.env.PORT || 3000);
 });
